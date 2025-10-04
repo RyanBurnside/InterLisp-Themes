62d513ae9e4791b393f3450b2b845e16299b5be0
Ryan Burnside <pixeloutlaw@gmail.com>
2025-10-03 22:29:03 -0600

    Added my initial themes. I like 70sBanknote personally.

---
+++ medley.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+# 70s Banknote
+# medley --apps --noscroll --geometry 1920x1080 --screensize 1920x1080 --  -cursorColor orangered -fg '#402e09' -bg ivory
+
+# Blue LCD
+# medley --apps --noscroll --geometry 1920x1080 --screensize 1920x1080 --  -cursorColor black -fg midnightblue -bg lightgray
+
+# Evening Lilac
+# medley --apps --noscroll --geometry 1920x1080 --screensize 1920x1080 --  -cursorColor black -fg indigo -bg lavender
+
+# What a Maroon
+# medley --apps --noscroll --geometry 1920x1080 --screensize 1920x1080 --  -cursorColor black -fg webmaroon -bg lightyellow
+
+# Seafoam
+# medley --apps --noscroll --geometry 1920x1080 --screensize 1920x1080 --  -cursorColor black -fg seagreen -bg floralwhite
+
+# Hospital Scrubs
+# medley --apps --noscroll --geometry 1920x1080 --screensize 1920x1080 --  -cursorColor black -fg teal -bg seashell
+
+# Candycane
+# medley --apps --noscroll --geometry 1920x1080 --screensize 1920x1080 --  -cursorColor black -fg darkred -bg white
+
+# Blueprint
+# medley --apps --noscroll --geometry 1920x1080 --screensize 1920x1080 --  -cursorColor black -fg gainsboro -bg darkslateblue
+
+# Amber Phosphor
+# medley --apps --noscroll --geometry 1920x1080 --screensize 1920x1080 --  -cursorColor gold -fg goldenrod -bg black
+
+# Eye B. M. Phosphor
+# medley --apps --noscroll --geometry 1920x1080 --screensize 1920x1080 --  -cursorColor white -fg palegreen -bg black
+
+# Debbie's 80s Tracksuit
+# medley --apps --noscroll --geometry 1920x1080 --screensize 1920x1080 --  -cursorColor black -fg lavenderblush -bg rebeccapurple
